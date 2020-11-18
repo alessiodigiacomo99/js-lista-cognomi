b32afc61f6906725e917ebae8ca2a33e492b1f0b
alessio <alessiodg99.adg@gmail.com>
2020-11-18 16:28:58 +0100

    fine cognomi

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Chiedere all'utente il cognome
+var cognome = prompt("Qual è il tuo cognome?");
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+var cognomeCapitalized = cognome.charAt(0).toUpperCase() + cognome.slice(1)
+
+// Inserirlo in un array con altri cognomi
+listaCognomi.push(cognomeCapitalized);
+
+// Stampa la lista ordinata alfabeticamente
+listaCognomi.sort()
+console.log(listaCognomi);
+
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+for (i = 0; i < listaCognomi.length; i++) {
+    if(cognomeCapitalized == listaCognomi[i]){
+        console.log(i + 1);
+        
+    }
+}
